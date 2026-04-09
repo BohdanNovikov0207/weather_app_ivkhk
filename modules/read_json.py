7d4ab5efa138bf74df0747ad4ded61194bca8b3e
Pavel Kut�rin <pavel.kutorin@ivkhk.ee>
2026-04-09 10:21:16 +0300

    Description for read_json func

--- modules/read_json.py
+++ modules/read_json.py
@@ -1,7 +1,10 @@
 import os, json
 
+# loeb fail mis on kirjutatud
 def read(file_name: str):
+    # vastutab funktsiooniparameetris määratud faili tee genereerimise eest ja salvestab selle muutujasse
     path = os.path.abspath(__file__ + '/../../static/' + file_name)
+    # avab fail teel mis on määratud path`is ja tagastab file mis on töödeldus sõnastikuna
     with open(path) as file:
         return json.load(file)
     
